f5a38909d603ef4cee8487bc16f079d5b7569804
spiros_kraken <uberi12345@hotmail.com>
2018-10-15 18:46:06 +0300

    Updates

---
+++ precache-manifest.3c774adb1af15b034ee88ae38c9d2356.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/RecipeApp/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "62829c84a9ccf5d07174",
+    "url": "/RecipeApp/static/js/runtime~main.62829c84.js"
+  },
+  {
+    "revision": "47cfad70d8b0e491a0d3",
+    "url": "/RecipeApp/static/js/main.47cfad70.chunk.js"
+  },
+  {
+    "revision": "6105a37ce77693a684b2",
+    "url": "/RecipeApp/static/js/1.6105a37c.chunk.js"
+  },
+  {
+    "revision": "47cfad70d8b0e491a0d3",
+    "url": "/RecipeApp/static/css/main.aa97dd10.chunk.css"
+  },
+  {
+    "revision": "259d46e83d0c4ba9c6598454a541fff8",
+    "url": "/RecipeApp/index.html"
+  }
+];
